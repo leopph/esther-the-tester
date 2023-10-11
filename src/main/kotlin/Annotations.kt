4441a28257a677a01bded4c74d0e737f2a78c54a
Levente Löffler <levente.loffler99@gmail.com>
2023-10-11 10:54:57 +0200

    Add annotation classes

---
+++ src/main/kotlin/Annotations.kt
@@ -0,0 +1,14 @@
+@Target(AnnotationTarget.CLASS)
+annotation class TestCase
+@Target(AnnotationTarget.FUNCTION)
+annotation class BeforeClass
+@Target(AnnotationTarget.FUNCTION)
+annotation class AfterClass
+@Target(AnnotationTarget.FUNCTION)
+annotation class Before
+@Target(AnnotationTarget.FUNCTION)
+annotation class After
+@Target(AnnotationTarget.FUNCTION)
+annotation class Test
+@Target(AnnotationTarget.FUNCTION)
+annotation class Skip
